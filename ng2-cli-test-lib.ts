aa7607b32f2e261c9ff2ca820ac607e550ff21ee
Nathan Walker <walkerrunpdx@gmail.com>
2015-12-11 00:35:45 -0800

    ready

--- ng2-cli-test-lib.ts
+++ ng2-cli-test-lib.ts
@@ -1,8 +1,10 @@
-export {TestService, TestService2} from './src/app/services/test.service';
-
 import {TestDirective} from './src/app/directives/test.directive';
 import {TestService, TestService2} from './src/app/services/test.service';
 import {TestPipe} from './src/app/pipes/test.pipe';
+
+export * from './src/app/directives/test.directive';
+export * from './src/app/services/test.service';
+export * from './src/app/pipes/test.pipe';
 
 export default {
   providers: [TestService, TestService2],
